721623dc77f83a54d116ce4409fd7abbbcd413ca
Brunorakotomalala-espa <brunorakotomalala113@gmail.com>
2024-09-22 08:40:48 +0300

    Update handleMessage.js

--- handles/handleMessage.js
+++ handles/handleMessage.js
@@ -1,13 +1,37 @@
-const sendMessage = require('./sendMessage');
+const fs = require('fs');
+const path = require('path');
 
-function handleMessage(event) {
-    const senderPsid = event.sender.id;
-    const receivedMessage = event.message;
+// Charger toutes les commandes dans le répertoire commands
+const commands = {};
+const commandFiles = fs.readdirSync(path.join(__dirname, '../commands')).filter(file => file.endsWith('.js'));
 
-    if (receivedMessage && receivedMessage.text) {
-        const userMessage = receivedMessage.text;
-        sendMessage(senderPsid, `You sent the message: "${userMessage}"`);
-    }
+for (const file of commandFiles) {
+    const command = require(`../commands/${file}`);
+    commands[command.config.name] = command;
 }
 
-module.exports = handleMessage;
+module.exports = function handleMessage(sender_psid, received_message, callSendAPI) {
+    // Vérifier si le message contient du texte
+    if (received_message.text) {
+        const messageText = received_message.text.toLowerCase();
+
+        // Extraire la commande et les arguments
+        const [commandName, ...args] = messageText.split(' ');
+
+        // Vérifier si la commande existe dans les fichiers du répertoire commands
+        if (commands[commandName]) {
+            const command = commands[commandName];
+
+            // Appeler la fonction onStart de la commande correspondante avec les paramètres simplifiés
+            command.onStart(sender_psid, args.join(' '), (responseText) => {
+                callSendAPI(sender_psid, { text: responseText }); // Envoyer la réponse au bot
+            });
+        } else {
+            // Si la commande n'existe pas, envoyer une réponse par défaut
+            const response = {
+                text: `La commande "${commandName}" n'existe pas. Essayez une commande valide.`
+            };
+            callSendAPI(sender_psid, response);
+        }
+    }
+};
